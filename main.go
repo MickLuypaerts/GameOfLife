d7d239161e9c99179a99e41b817c3b7ab6d69c0a
Mick Luypaerts <mickluypaerts@gmail.com>
2021-06-18 19:13:26 +0200

    Added function to auto open browser on start up

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"./game"
 	"./handlers"
+	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
@@ -12,5 +15,29 @@ func main() {
 	port := ":3000"
 	mux := handlers.Mux(gameState)
 	log.Printf("Starting the server on %s\n", port)
+	err := openbrowser("http://localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Fatal(http.ListenAndServe(port, mux))
+}
+
+func openbrowser(url string) error {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
 }
